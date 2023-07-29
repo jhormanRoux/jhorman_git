5023e975223ceeaa5b6718f532a06f26f132ff27
jhorman <deiby190@gmail.com>
2023-07-28 20:25:00 -0500

    modificacion3

--- promedio.js
+++ promedio.js
@@ -2,5 +2,6 @@ let number1 = parseInt(prompt('Ingrese un numero'))
 let number2 = parseInt(prompt('Ingrese otro un numero'));
 
 function promedio() {
-    
+    return (number1 + number2 ) / 2
 }
+document.write(promedio());
